4459385926fb7ba96104a64237bf6fc8a35101ba
zhiqiangxu <652732310@qq.com>
2024-04-15 23:37:04 +0800

    support estimate gas for blob tx

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -664,6 +664,14 @@ func toCallArg(msg ethereum.CallMsg) interface{} {
 	if msg.GasTipCap != nil {
 		arg["maxPriorityFeePerGas"] = (*hexutil.Big)(msg.GasTipCap)
 	}
+	if msg.AccessList != nil {
+		arg["accessList"] = msg.AccessList
+	if msg.BlobGasFeeCap != nil {
+		arg["maxFeePerBlobGas"] = (*hexutil.Big)(msg.BlobGasFeeCap)
+	}
+	if msg.BlobHashes != nil {
+		arg["blobVersionedHashes"] = msg.BlobHashes
+	}
 	return arg
 }
 
